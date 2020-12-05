7a71917a4758cfff5acb8fed129274e4f0ee11aa
Floupy <tmurnieks@gmail.com>
2020-12-05 12:49:46 +0200

    version 12:50

---
+++ SpaceInvaders/Spaceship.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace SpaceInvaders
+{
+    class Spaceship : PictureBox
+    {
+        public Spaceship()
+        {
+            InitializeSpaceship();
+            
+
+        }
+
+        public void InitializeSpaceship()
+        {
+            this.Height = 100;
+            this.Width = 60;
+            this.BackColor = Color.SteelBlue;
+
+        }
+
+    }
+
+}
+
